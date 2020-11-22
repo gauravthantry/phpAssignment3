8fca4e4dd4f0273d41e2e1f0f3d43030f11efe85
Gaurav Thantry <gaurav.thantry@gmail.com>
2020-11-22 16:14:45 +1300

    Modifed kandingPages to fetch ini and call the navBar constructor

--- pages/landingPage-hindi.php
+++ pages/landingPage-hindi.php
@@ -1,41 +1,24 @@
+<?php
+require_once "../components/head.php";
+require_once "../components/navBar.php";
+?>
 <!DOCTYPE html>
 <html>
-
-<head>
-    <Title>गेमिंग समुदाय</Title>
-    <link href="https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../resource/app.css">
-    <link rel="stylesheet" href="../resource/semantic_ui/semantic.min.css">
-
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
-</head>
+<?php
+ $head = new Head('गेमिंग समुदाय');
+ $head->formHead();
+?>
 
 <body class="root">
     <?php
-    $ini_array = parse_ini_file( '../ini/landing.ini',true);
+    
+    ?>        <!-- <--- Assessment 1: 1 php tag, escaping html, instruction separation -->
+    <div class='container'>
+    <?php    
+    $ini_array = parse_ini_file( '../ini/navbar.ini',true);  //<---- Assessment 1: 18 (il8n)
     $locale="hi";
-    echo "<div class='container'>";
-       echo "<div class='ui inverted menu'>";
-           echo "<a class='active item'>";
-                echo $ini_array[$locale]["Home"];
-            echo "</a>";
-            echo "<a class='item' href='./viewPosts.html'>";
-               echo $ini_array[$locale]["View-Posts"];
-            echo "</a>";
-            echo "<a class='item' href='./postForm.html'>";
-               echo $ini_array[$locale]["New-Post"];
-           echo "</a>";
-           echo "<div class='right menu'>";
-               echo "<a class='item'>";
-                   echo $ini_array[$locale]["Login"];
-               echo "</a>";
-               echo "<a class='item'>";
-                   echo $ini_array[$locale]["Register"];
-               echo "</a>";
-           echo "</div>";
-       echo "</div>";
+    $navBar = new NavBar("home",$ini_array,"hi");
+    $navBar->formNavBar();
        echo "<div class='body-content'>";
            echo "<h2 class='welcome-message'>".$ini_array[$locale]["Welcome-Message"]."</h2>";
        echo "</div>";
@@ -44,3 +27,5 @@
 </body>
 
 </html>
+
+
